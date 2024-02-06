6375dc0e8f7e68b84afa0ba93d8e09b210a87440
Andre Queiroz Machado <de.machado90@gmail.com>
2024-02-06 22:00:27 +0000

    Week 02

---
+++ weekTwo/addOne.py
@@ -0,0 +1,7 @@
+# add one
+# author name: Andre Machado
+# prompt the user for a number and output plus one
+
+number = int(input('please enter a number: '))
+newNumber = number + 1
+print(f'{number} plus one is {newNumber}')
---
+++ weekTwo/hello2.py
@@ -0,0 +1,10 @@
+# Say name
+# author name: Andre Machado
+# prompt the user for name and output
+
+name = input("Enter your name: ")
+print("Hello " + name + '\nNice to meet you')
+
+# or this format
+
+print(f'Hello {name} \nNice to meet you')
