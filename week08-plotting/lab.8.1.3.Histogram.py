7f3318802331d7c8d3b33043277b1622c48f361e
Andre Queiroz Machado <de.machado90@gmail.com>
2024-04-29 11:06:15 +0100

    Weekly tasks

--- week08-plotting/lab.8.1.3.Histogram.py
+++ week08-plotting/lab.8.1.3.Histogram.py
@@ -10,21 +10,21 @@ min_salary = 20000
 random_number = 10
 
 np.random.seed(1)
-salaries = np.random.randint(max_salary, min_salary, random_number)
+salaries = np.random.randint(min_salary, max_salary, random_number)
 plt.hist(salaries)
-plt.show()
+#plt.show()
 #Write a program that makes a list called ages that has, the same number random
 #values as salaries, (range:21 to 65) . Make scatter plot of this data.
 ages = np.random.randint(low=21, high=65, size =random_number)
 plt.scatter(ages, salaries)
-plt.show()
+#plt.show()
 
 #Add a line to this plot that shows y= x2 in a different colour
 xpoints = np.array(range(1,101))
 ypoints = xpoints * xpoints
 
 plt.plot(xpoints, ypoints, color="r")
-plt.show()
+#plt.show()
 
 #Add a legend, title and axis labels to this plot
 plt.title("Random plot")
